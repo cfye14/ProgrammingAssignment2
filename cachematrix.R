c9151bd52a2bb564c9ac721a4c710a07c665e443
cfye14 <cfye14nb9x@gmail.com>
2015-01-23 22:55:09 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 ## original matrix or its inverse, and perform the inverse operation on the cached matrix.
 
 ## The makeCacheMatrix function caches a matrix. The enclosed functions allow the user to re-set the value of the parameter matrix,
-## retrieve the original matrix, assign a value for a (the) inverse matrix, and retrieve the inverse matrix. 
+## retrieve the matrix, assign a value for a (the) inverse matrix, and retrieve the inverse matrix. 
 ## The output is a list of the enclosed functions.
 
 makeCacheMatrix <- function(x) {
